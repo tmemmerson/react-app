1dbdfd413880fb8137218dbfc56a5235df36f12b
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 11:22:16 -0700

    create drink component

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,1 +1,28 @@
- 
+import React from "react";
+import PropTypes from "prop-types";
+
+function Drink(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenDrinkClicked(props.id)}>
+        <h3>
+          {props.brand} - {props.names}
+        </h3>
+        <p>
+          <em>{props.flavor}</em>
+        </p>
+        <hr />
+      </div>
+    </React.Fragment>
+  );
+}
+
+Drink.propTypes = {
+  names: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  flavor: PropTypes.string,
+  id: PropTypes.string,
+  whenDrinkClicked: PropTypes.func,
+};
+
+export default Drink;
