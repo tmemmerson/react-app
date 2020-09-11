c3b59bd566ad37e9bd5602d5448fd80f136f0eeb
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 11:39:53 -0700

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./Header";
+import DrinkControl from "./DrinkControl";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Container
+        style={{
+          backgroundColor: "rgba(245, 245, 245, 0.5)",
+          marginTop: "150px",
+        }}
+      >
+        <DrinkControl />
+      </Container>
+    </React.Fragment>
   );
 }
 
