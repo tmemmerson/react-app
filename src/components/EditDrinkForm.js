cb5b815389212bee6ddd71dd5a64230ae3f2f23a
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 13:20:41 -0700

    create edit drink form component

--- src/components/EditDrinkForm.js
+++ src/components/EditDrinkForm.js
@@ -1,1 +1,33 @@
- 
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditDrinkForm(props) {
+  const { drink } = props;
+  function handleEditDrinkFormSubmission(event) {
+    event.preventDefault();
+    props.onEditDrink({
+      names: event.target.names.value,
+      brand: event.target.brand.value,
+      flavor: event.target.flavor.value,
+      price: event.target.price.value,
+      tap: event.target.tap.value,
+      id: drink.id,
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditDrinkFormSubmission}
+        buttonText="Update Drink"
+      />
+    </React.Fragment>
+  );
+}
+
+EditDrinkForm.propTypes = {
+  onEditDrink: PropTypes.func,
+};
+
+export default EditDrinkForm;
