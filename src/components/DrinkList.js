d94c5ae111d32316acf5ec6f9ec262ba7fa0f5b0
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 13:16:03 -0700

    create drink list component

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,1 +1,30 @@
- 
+import React from "react";
+import Drink from "./Drink";
+import PropTypes from "prop-types";
+
+function DrinkList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.drinkList.map((drink) => (
+        <Drink
+          whenDrinkClicked={props.onDrinkSelection}
+          names={drink.names}
+          brand={drink.brand}
+          flavor={drink.flavor}
+          price={drink.price}
+          tap={drink.tap}
+          id={drink.id}
+          key={drink.id}
+        />
+      ))}
+    </React.Fragment>
+  );
+}
+
+DrinkList.propTypes = {
+  drinkList: PropTypes.array,
+  onDrinkSelection: PropTypes.func,
+};
+
+export default DrinkList;
