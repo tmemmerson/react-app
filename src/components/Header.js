32b264cd1c3537e258b70caa1a5339fe60074a43
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 13:30:01 -0700

    update link titles cause i think im funny

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,17 +17,20 @@ function Header(props) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Drinks</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link href="#home">vuoto</Nav.Link>
+            <Nav.Link href="#link">branco</Nav.Link>
+            <Nav.Link href="#link">vazio</Nav.Link>
+            <NavDropdown title="These links" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                don't go anywhere
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                but you have
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">to admit</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                they're pretty
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
