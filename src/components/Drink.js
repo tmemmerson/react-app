56911a4c45c57883a254ae39a7e0784266599402
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 13:15:19 -0700

    update drink component

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -11,6 +11,12 @@ function Drink(props) {
         <p>
           <em>{props.flavor}</em>
         </p>
+        <p>
+          <em>${props.price}</em>
+        </p>
+        <p>
+          <em>On tap: {props.tap}(ounces)</em>
+        </p>
         <hr />
       </div>
     </React.Fragment>
@@ -21,6 +27,8 @@ Drink.propTypes = {
   names: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   flavor: PropTypes.string,
+  price: PropTypes.number,
+  tap: PropTypes.number,
   id: PropTypes.string,
   whenDrinkClicked: PropTypes.func,
 };
