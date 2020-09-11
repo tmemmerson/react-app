b9ea41f6ae4d8d57ec06f0a05abdc9c74ae6f74b
Tristan Emmerson <tristan@stickerslug.com>
2020-09-11 13:20:04 -0700

    create new drink form component

--- src/components/NewDrinkForm.js
+++ src/components/NewDrinkForm.js
@@ -1,1 +1,33 @@
- 
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewDrinkForm(props) {
+  function handleNewDrinkFormSubmission(event) {
+    event.preventDefault();
+    props.onNewDrinkCreation({
+      names: event.target.names.value,
+      brand: event.target.brand.value,
+      flavor: event.target.flavor.value,
+      price: event.target.price.value,
+      tap: event.target.tap.value,
+      id: v4(),
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewDrinkFormSubmission}
+        buttonText="Add Drink"
+      />
+    </React.Fragment>
+  );
+}
+
+NewDrinkForm.propTypes = {
+  onNewDrinkCreation: PropTypes.func,
+};
+
+export default NewDrinkForm;
